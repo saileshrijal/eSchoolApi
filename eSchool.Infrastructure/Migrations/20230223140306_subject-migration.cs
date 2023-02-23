f01d634899c2e06432e2b8ef5bd38ec7b430da9f
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-02-23 21:47:08 +0545

    subject module completed.

---
+++ eSchool.Infrastructure/Migrations/20230223140306_subject-migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eSchool.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class subjectmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
